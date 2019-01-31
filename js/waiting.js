c70feff7559954bfb76f7789eac89a49c53f2fe4
junniejobs <junniejobs@gmail.com>
2019-02-01 00:22:02 +0100

    refactor(waiting.js): Remove unused console.log

--- js/waiting.js
+++ js/waiting.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
         "use strict";
         var height = $('#waiting-apply-from').height()
         if(window.innerWidth <= 480) {
-            console.log(height)
             $('.google-form-container').height('725px')
             $('#waiting-apply-from').height('725px')
         } else {
