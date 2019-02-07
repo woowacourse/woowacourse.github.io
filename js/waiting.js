b11a93e0e62c3c9c759d33048896a20ada626c91
ExplorerJun <46333340+ExplorerJun@users.noreply.github.com>
2019-02-07 19:29:30 +0100

    refactor(waitingApply.html): Change bg image and set iframe's height (#2)

--- js/waiting.js
+++ js/waiting.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     $(function(){
         "use strict";
         var height = $('#waiting-apply-from').height()
+
         if(window.innerWidth <= 480) {
-            $('.google-form-container').height('725px')
-            $('#waiting-apply-from').height('725px')
+            $('.google-form-container').height('780px')
+            $('#waiting-apply-from').height('780px')
         } else {
             $('.google-form-container').height(`${height}px`)
         }
