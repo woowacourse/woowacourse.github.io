d5fa3a185729304ea3d61881934217dc875d5e1b
eastjun <eastjun@woowahan.com>
2019-10-07 15:51:45 +0900

    refactor(app.js): refactor convention & es6

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const COMING_SOON_APP = (() => {
     const randomizeBackgroundUrl = () => {
         const randomNum = Math.floor(Math.random() * 4)
         const bgUrl = `/images/bg/${randomNum}.jpeg`
-        $('#main-bg').css('background-image', `url('${bgUrl}')`);
+        $('#main-bg').css('background-image', `url('${bgUrl}')`)
     }
 
     const setCountdown = () => {
@@ -12,79 +12,66 @@ const COMING_SOON_APP = (() => {
     }
 
     const countdown = () => {
-        $.fn.countdownTimer = function( options ) {
-
-
-            // This is the easiest way to have default options.
-            var settings = $.extend({
+        $.fn.countdownTimer = (options) => {
+            const settings = $.extend({
                 endTime: new Date()
-            }, options );
+            }, options )
 
-            var $this = $(this);
+            const $this = $(this)
 
-            var $seconds = $this.find(".time.seconds");
-            var $minutes = $this.find(".time.minutes");
-            var $hours = $this.find(".time.hours");
-            var $days = $this.find(".time.days");
+            const $seconds = $this.find(".time.seconds")
+            const $minutes = $this.find(".time.minutes")
+            const $hours = $this.find(".time.hours")
+            const $days = $this.find(".time.days")
 
-            var seconds = 0;
-            var minutes = 0;
-            var days = 0;
-            var hours = 0;
+            let seconds = 0
+            let minutes = 0
+            let days = 0
+            let hours = 0
 
-            var switchCountdownValue = function ($obj, val) {
-                // Add leading zero
-                var s = val+"";
-                while (s.length < 2) s = "0" + s;
+            const switchCountdownValue = ($obj, val) => {
+                let s = val + "";
+                while (s.length < 2) s = "0" + s
 
-                if(Modernizr.cssanimations) {
-                    // Fade out previous
-                    var prev = $obj.find(".value").addClass("fadeOutDown").addClass("animated");
-
-                    // Add next value
-                    var next = $("<div class='value'>" + s + "</div>");
-                    $obj.prepend(next);
-                    // Fade in next value
-                    next.addClass("fadeInDown").addClass("animated");
-                    // Remove from DOM on animation end
-                    // Fix for Safari (if window is not active, then webkitAnimationEnd doesn't fire, so delete it on timeout)
-                    var to = setTimeout(function(){ prev.remove() }, 200);
-                    prev.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                if (Modernizr.cssanimations) {
+                    const prev = $obj.find(".value").addClass("fadeOutDown").addClass("animated")
+                    const next = $("<div class='value'>" + s + "</div>")
+                    $obj.prepend(next)
+                    next.addClass("fadeInDown").addClass("animated")
+                    const to = setTimeout(function(){ prev.remove() }, 200)
+                    prev.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
                         prev.remove();
                         clearTimeout(to);
                     });
 
                 } else {
-                    // Remove previous value
-                    var prev = $obj.find(".value").remove();
-                    // Add next value
-                    var next = $("<div class='value'>" + s + "</div>");
+                    const prev = $obj.find(".value").remove();
+                    const next = $("<div class='value'>" + s + "</div>");
                     $obj.prepend(next);
                 }
             }
 
-            var timerId = countdown(settings.endTime,
-                function(ts) {
-                    if(seconds != ts.seconds) {
+            const timerId = countdown(settings.endTime,
+                (ts) => {
+                    if (seconds != ts.seconds) {
                         switchCountdownValue($seconds, ts.seconds);
                         seconds = ts.seconds;
                     }
-                    if(minutes != ts.minutes) {
+                    if (minutes != ts.minutes) {
                         switchCountdownValue($minutes, ts.minutes);
                         minutes = ts.minutes;
                     }
-                    if(hours != ts.hours) {
+                    if (hours != ts.hours) {
                         switchCountdownValue($hours, ts.hours);
                         hours = ts.hours;
                     }
 
-                    if(days != ts.days) {
+                    if (days != ts.days) {
                         switchCountdownValue($days, ts.days);
                         days = ts.days;
                     }
                 },
-                countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS);
-
+                countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
 
             return this;
 
