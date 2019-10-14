7b6a28a680048eac58c9f84c8621c7fb3ac9aa59
임동준 <eastjun@woowahan.com>
2019-10-14 15:00:09 +0900

    Feat/before apply open (#278)
    * refactor(assets): remove unused files
    
    * feat(faq): add FAQ page
    
    * fix(faq.html): Reflect the updated FAQ.
    
    * refactor(FAQ): update jquery version
    
    * refactor(index): refactor duplicated html code
    
    * refactor(footer): refactor footer class name
    
    * refactor(contact): optimize banner image
    
    * refactor(contact): remove unused text tag
    
    * refactor(faq): refactor faq css style
    
    * refactor(footer): add github & youtube url
    
    * refactor(index): create html template to remove duplicated html parts
    
    * refactor(curriculum): remove duplicated codes
    
    * refactor(preloader): create template for preloader
    
    * refactor(index): add eligibility image01
    
    * refactor(curriculum): add list style
    
    * refactor(googleAnalytics): add ga load script
    
    * refactor(index): refactor index page style & image
    
    * refactor(faq): refactor the answer mismatched
    
    * refactor(faq.js): refactor text indent
    
    * refactor(index): rearrange bg images
    
    * refactor(response.css): refactor menu text color when hover
    
    * refactor(cycle-image): update cycle image
    
    * refactor(app.js): refactor common script function
    
    * refactor(app.js): remove unused eventListner
    
    * refactor(contact): remove unused codes
    
    * refactor(curriculum): change background image
    
    * refactor(curriculum): update background image
    
    * refactor(schedule): refactor schedule text spacing
    
    * refactor(index): rearrange & repositioning background images
    
    * refactor(index): add dot after text explain
    
    * refactor(faq): update answer
    
    * refactor(faq): refactor text grammer

--- js/app.js
+++ js/app.js
@@ -1,92 +1,66 @@
-const COMING_SOON_APP = (() => {
-    const randomizeBackgroundUrl = () => {
-        const randomNum = Math.floor(Math.random() * 4)
-        const bgUrl = `/images/bg/${randomNum}.jpeg`
-        $('#main-bg').css('background-image', `url('${bgUrl}')`)
+const APP = (() => {
+    'use strict';
+
+    const loadHeader = () => {
+        fetch('../include/header.html')
+        .then(response => { return response.text() })
+        .then(header => document.querySelector('#header-template').innerHTML = header)
     }
 
-    const setCountdown = () => {
-        $(".countdown").countdownTimer({
-            endTime: new Date("October 14, 2019 15:00:00 UTC+0900")
-        })
+    const loadFooter = () => {
+        fetch('../include/footer.html')
+        .then(response => { return response.text() })
+        .then(footer => document.querySelector('#footer-template').innerHTML = footer)
     }
 
-    const countdown = () => {
-        $.fn.countdownTimer = (options) => {
-            const settings = $.extend({
-                endTime: new Date()
-            }, options )
+    const initNavbar = () => {
+        window.addEventListener('scroll', () => {
+            const scroll = window.scrollY
+            const headerAreaClassList = document.querySelector('.header_area').classList
+            scroll ? headerAreaClassList.add('navbar_fixed') : headerAreaClassList.remove('navbar_fixed')
+        });
+    }
 
-            const $this = $(this)
+    const initPreloader = () => {
+        const preloader = document.getElementById('ctn-preloader')
+        preloader.classList.add('loaded')
+        if (preloader.classList.contains('loaded')) {
+            setTimeout(() => {
+                document.getElementById('ctn-preloader').remove()
+            }, 500)
+        }
+    }
 
-            const $seconds = $this.find(".time.seconds")
-            const $minutes = $this.find(".time.minutes")
-            const $hours = $this.find(".time.hours")
-            const $days = $this.find(".time.days")
-
-            let seconds = 0
-            let minutes = 0
-            let days = 0
-            let hours = 0
-
-            const switchCountdownValue = ($obj, val) => {
-                let s = val + "";
-                while (s.length < 2) s = "0" + s
-
-                if (Modernizr.cssanimations) {
-                    const prev = $obj.find(".value").addClass("fadeOutDown").addClass("animated")
-                    const next = $("<div class='value'>" + s + "</div>")
-                    $obj.prepend(next)
-                    next.addClass("fadeInDown").addClass("animated")
-                    const to = setTimeout(function(){ prev.remove() }, 200)
-                    prev.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', () => {
-                        prev.remove();
-                        clearTimeout(to);
-                    });
-
-                } else {
-                    const prev = $obj.find(".value").remove();
-                    const next = $("<div class='value'>" + s + "</div>");
-                    $obj.prepend(next);
-                }
-            }
-
-            const timerId = countdown(settings.endTime,
-                (ts) => {
-                    if (seconds != ts.seconds) {
-                        switchCountdownValue($seconds, ts.seconds);
-                        seconds = ts.seconds;
-                    }
-                    if (minutes != ts.minutes) {
-                        switchCountdownValue($minutes, ts.minutes);
-                        minutes = ts.minutes;
-                    }
-                    if (hours != ts.hours) {
-                        switchCountdownValue($hours, ts.hours);
-                        hours = ts.hours;
-                    }
-
-                    if (days != ts.days) {
-                        switchCountdownValue($days, ts.days);
-                        days = ts.days;
-                    }
-                },
-                countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
-
-            return this;
-
-        };
+    const initMainImgSlider = () => {
+        const slider = $(".main_slider")
+        if (slider.length) {
+            slider.owlCarousel({
+                loop:true,
+                margin:0,
+                items: 1,
+                dots: false,
+                nav:false,
+                autoplay: true,
+                slideSpeed: 300,
+                mouseDrag: false,
+                animateIn: 'fadeIn',
+                animateOut: 'fadeOut',
+                responsiveClass:true,
+            })
+        }
     }
 
     const init = () => {
-        randomizeBackgroundUrl()
-        setCountdown()
-        countdown()
+        loadHeader()
+        initNavbar()
+        initMainImgSlider()
+        loadFooter()
+        initPreloader()
     }
 
     return {
-        init: init
+        init: init,
     }
 })()
 
-COMING_SOON_APP.init()
+APP.init()
---
+++ js/index.js
@@ -0,0 +1,88 @@
+const INDEX_APP = (() => {
+    'use strict';
+
+    const bannerBgTemplate = (index) => {
+        let bgPosition
+        switch (index) {
+            case 2:
+                bgPosition = '17.5% 100%'
+                break
+            case 3:
+                bgPosition = '60% 100%'
+                break
+            default:
+                bgPosition = 'center'
+        }
+        return `
+                <div class="main_banner_item" style="background: no-repeat ${bgPosition}/cover url(images/pages/index/banner-bg${index}.jpg);"></div>
+               `
+    }
+
+    const scheduleTemplate = (schedule, index) => {
+        const scheduleNum = index + 1
+        const separator = `<div class="separator"></div>`
+        return `
+                <div class="col-lg-2 col-sm-6">
+                    <div class="schedule_item text-center">
+                        <div class="number">${scheduleNum}</div>
+                        ${scheduleNum === 1 ? '' : separator}
+                        <div class="new_calendar_content">
+                            <h4 class="f_size_20 f_p f_500">${schedule.title}</h4>
+                            <p class="f_400 f_size_15 mb-0">${schedule.content}</p>
+                        </div>
+                    </div>
+                </div>
+                `
+    }
+
+    const createSchedule = () => {
+        const schedules = [
+            {
+                title: '서류지원',
+                content: '10월 25일(금) ~ 11월 5일(화) 10시'
+            },
+            {
+                title: '온라인 코딩 테스트',
+                content: '11월 9일(토)'
+            },
+            {
+                title: '1차 합격자 발표',
+                content: '11월 20일(수) 15시'
+            },
+            {
+                title: '프리코스',
+                content: '11월 27일(수) ~ 12월 18일(수)'
+            },
+            {
+                title: '최종 테스트',
+                content: '12월 21일(토)'
+            },
+            {
+                title: '최종 합격자 발표',
+                content: '2020년 1월 3일(금) 15시'
+            }
+        ]
+        const scheduleContainer = document.getElementById('calender-container')
+        schedules.map((schedule, index) => scheduleContainer.insertAdjacentHTML('beforeend', scheduleTemplate(schedule, index)))
+    }
+
+    const createBannerBgImages = () => {
+        const bannerBgNum = 9
+        let index = 1
+
+        while (index < bannerBgNum) {
+            document.getElementById('banner-cover-container').insertAdjacentHTML('afterbegin', bannerBgTemplate(index++))
+        }
+    }
+
+    const init = () => {
+        createBannerBgImages()
+        createSchedule()
+    }
+
+    return {
+        init: init,
+    }
+})()
+
+INDEX_APP.init()
